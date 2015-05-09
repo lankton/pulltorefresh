74da38641a584e1eb8db8f1ab68e78fc4a55e554
lankton <taofangxin2011@163.com>
2015-05-10 00:04:47 +0800

    initial commit

---
+++ src/com/lankton/pulltorefresh/MainActivity.java
@@ -0,0 +1,135 @@
+package com.lankton.pulltorefresh;
+
+import java.util.ArrayList;
+
+import com.lankton.pulltorefresh.R;
+import com.lankton.pulltorefresh.PTRScrollView.OnPullListener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends Activity {
+
+    private ListView lv;
+    private PTRScrollView sv;
+    private TextView head_text;
+    private ImageView head_img;
+    private MHandler handler;
+    private Animation anim;
+    private int nextIndex = 0;
+    
+    private static ArrayList<Character> charList = new ArrayList<Character>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        head_text = (TextView) this.findViewById(R.id.head_text);
+        head_img = (ImageView) this.findViewById(R.id.head_img);
+        
+        anim = AnimationUtils.loadAnimation(this, R.anim.retate);
+        anim.setFillAfter(true);
+        
+        sv = (PTRScrollView) this.findViewById(R.id.mPTRScrollView);
+        sv.setSmoothScrollingEnabled(true);
+        lv = (ListView) this.findViewById(R.id.listview);
+        handler = new MHandler();
+        for(int i = 0; i < 3; i++)
+        {
+            charList.add((char) ('a' + i));
+        }
+        nextIndex = 3;
+        
+        lv.setAdapter(new ArrayAdapter<Character>(this,
+                android.R.layout.simple_list_item_1, charList));
+        lv.setSelection(0);
+        sv.reSize(lv);
+        
+        sv.setOnPullListener(new OnPullListener(){
+
+            @Override
+            public void onPull(int progress, int action) {
+                // TODO Auto-generated method stub
+                
+                if(progress == 100)
+                {
+                    if(MotionEvent.ACTION_UP == action || MotionEvent.ACTION_HOVER_EXIT == action)
+                    {
+                            head_img.startAnimation(anim);
+                            head_text.setText("loading...");
+                            new Thread(new Runnable(){
+
+                                @Override
+                                public void run() {
+                                    // TODO Auto-generated method stub
+                                    try {
+                                        charList.add((char) ('a' + nextIndex));
+                                        nextIndex ++;
+                                        Thread.sleep(3000);
+                                    } catch (InterruptedException e) {
+                                        // TODO Auto-generated catch block
+                                        e.printStackTrace();
+                                    }
+                                    handler.sendEmptyMessage(0);
+                                }
+                                
+                            }).start();
+                            
+                    } 
+                    else
+                    {
+                             head_text.setText("release to refresh");
+                    }
+                }
+                else
+                {
+                    if(MotionEvent.ACTION_UP == action)
+                    {
+                        sv.post(new Runnable() {@Override public void run() {sv.smoothScrollTo(0, 200);}});
+                    }
+                    else if(progress > 0)
+                    {
+                        {
+                            head_text.setText("pulled: " + progress + "%");
+                        }
+                    }
+                }
+                
+                
+            }
+            
+        });
+    }
+
+    
+    public class MHandler extends Handler{
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            if(0 == msg.what) {
+                ((ArrayAdapter)lv.getAdapter()).notifyDataSetChanged();
+                sv.reSize(lv);
+                head_img.clearAnimation();
+                sv.smoothHide();
+                sv.smoothHide();
+            }
+        }
+        
+        
+    }
+    
+    
+}
---
+++ src/com/lankton/pulltorefresh/PTRScrollView.java
@@ -0,0 +1,132 @@
+package com.lankton.pulltorefresh;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+public class PTRScrollView extends ScrollView{
+    
+    private String tag = "PTRScrollView";
+    private Context context;
+    private OnPullListener onPullListener;
+    private boolean isInit = true;
+    private int count = 0;
+    private int screenHeight = 0;
+
+    public PTRScrollView(Context context) {
+        super(context);
+        Log.i(tag, "construct with 1 param");
+        this.context = context;
+        // TODO Auto-generated constructor stub
+    }
+    public PTRScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Log.i(tag, "construct with 2 params");
+        this.context = context;
+        // TODO Auto-generated constructor stub
+    }
+    public PTRScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        Log.i(tag, "construct with 3 params");
+        this.context = context;
+        // TODO Auto-generated constructor stub
+    }
+
+    public ListView lv = null; 
+    public View headView = null;
+    public int headViewHeight;
+    
+    public int getScreenHeight()
+    {
+        if(0 == this.screenHeight)
+        {
+            WindowManager wm = (WindowManager)context
+                    .getSystemService(Context.WINDOW_SERVICE);
+            screenHeight = wm.getDefaultDisplay().getHeight();
+        }
+        return this.screenHeight;
+    }
+    
+    public void initView()
+    {
+        LinearLayout wrapper = (LinearLayout) this.getChildAt(0);
+        headView = wrapper.getChildAt(0);
+        lv = (ListView) wrapper.getChildAt(1);
+    }
+    
+    public void setOnPullListener(OnPullListener opl)
+    {
+        this.onPullListener = opl;
+    }
+    
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO Auto-generated method stub
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);        
+        initView();
+        headViewHeight = headView.getLayoutParams().height;
+        
+        isInit = false;
+    }
+    
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        // TODO Auto-generated method stub
+        int scrollY = this.getScrollY();
+        int progress = (headViewHeight - scrollY) * 100 / headViewHeight;
+        if(null != this.onPullListener) {
+            if(progress >= 0)
+            {Log.e(tag, String.valueOf(ev.getAction()));
+                this.onPullListener.onPull(progress, ev.getAction());
+            }
+        }
+        return super.onTouchEvent(ev);
+    }
+    
+    public void reSize(ListView lv) {  
+        ListAdapter listAdapter = lv.getAdapter();  
+        if (listAdapter == null) {  
+         return;  
+        }  
+        int totalHeight = 0;  
+        for (int i = 0; i < listAdapter.getCount(); i++) {  
+            View listItem = listAdapter.getView(i, null, lv);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();  
+        }  
+        
+        ViewGroup.LayoutParams params = lv.getLayoutParams();  
+        
+        params.height = totalHeight  
+          + (lv.getDividerHeight() * (listAdapter.getCount() - 1));  
+        
+        if(params.height < this.getScreenHeight())
+        {
+            params.height = this.screenHeight;
+        }
+        lv.setLayoutParams(params);  
+       }
+    
+    public void smoothHide()
+    {
+        this.scrollTo(0, this.headViewHeight);
+        this.smoothScrollTo(0, this.headViewHeight);
+    }
+
+    interface OnPullListener{
+        /*参数progress为下拉的从程度，<=0表示完全隐藏，100为完全拉下
+         * action为动作 */
+        void onPull(int progress, int action);
+    }
+
+}
